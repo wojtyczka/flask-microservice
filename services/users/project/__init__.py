a95ccecc7d06602868f657453ab5333f5d0ea400
Pawel Wojtyczka <pawel@promosms.com>
2018-11-03 06:55:03 +0100

    Adding new feature

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,18 @@
 # services/users/project/__init__.py
 
+import os
+import sys
 
 from flask import Flask, jsonify
-
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig') 
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+#print(app.config, file=sys.stderr)
 
 
 @app.route('/users/ping', methods=['GET'])
