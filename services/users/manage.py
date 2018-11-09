7223f73b80170b3ff32f58aac5fc0e3d5415488e
Paweł Wojtyczka <pawel@rebug.io>
2018-11-09 11:30:49 +0100

    New..

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,19 @@
 import unittest
+import coverage
 
 from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -21,7 +32,29 @@ def test():
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
-    return 1    
+    return 1
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+@cli.command()
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
